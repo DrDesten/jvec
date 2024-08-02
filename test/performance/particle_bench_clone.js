8a86b3f3a616bb2146adcc990ed27f4598703162
DrDesten <>
2024-08-02 15:30:13 +0200

    added particle clone bench

---
+++ test/performance/particle_bench_clone.js
@@ -0,0 +1,28 @@
+import { vec2 } from "../../bin/vec.js"
+import { PerformanceResult } from "./performance.js"
+
+class Particle {
+    constructor( pos ) {
+        this.pos = pos
+        this.vel = new vec2
+        this.acc = new vec2
+    }
+
+    update() {
+        this.vel = vec2.add( this.vel, this.acc )
+        this.pos = vec2.add( this.pos, this.vel )
+        this.acc = new vec2
+    }
+}
+
+export default function bench( particleCount, simulationSteps ) {
+    const start = performance.now()
+    const particles = Array.from( { length: particleCount }, () => new Particle( vec2.randomNorm() ) )
+    for ( let i = 0; i < simulationSteps; i++ ) {
+        for ( const particle of particles ) {
+            particle.update()
+        }
+    }
+    const end = performance.now()
+    return new PerformanceResult( "Particle Bench - 2D Clone", end - start, { particleCount, simulationSteps } )
+}
--- test/performance_bench.js
+++ test/performance_bench.js
@@ -1,4 +1,7 @@
 import ParticleInplace from "./performance/particle_bench_inplace.js"
+import ParticleClone from "./performance/particle_bench_clone.js"
 
 console.info( ParticleInplace( 10_000, 1_000 ).toString() )
 console.info( ParticleInplace( 10_000, 2_000 ).toString() )
+console.info( ParticleClone( 10_000, 1_000 ).toString() )
+console.info( ParticleClone( 10_000, 2_000 ).toString() )
